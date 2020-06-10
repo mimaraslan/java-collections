d97c4fd866a4eb03d4889b913d244b5ce5dc32b4
mimaraslan <mimaraslan@gmail.com>
2020-06-10 22:32:25 +0300

    Queue Interface
    Queue interface maintains the first-in-first-out order.

---
+++ src/com/mimaraslan/queue/MyArrayDeque.java
@@ -0,0 +1,48 @@
+package com.mimaraslan.queue;
+
+import java.util.*;
+
+
+public class MyArrayDeque {
+    public static void main(String args[]) {
+
+        Deque<String> myDeque = new ArrayDeque<String>();
+
+        myDeque.add("M");
+        myDeque.add("E");
+        myDeque.add("K");
+        myDeque.add("B");
+        myDeque.add("A");
+        myDeque.add("Z");
+        myDeque.add("C");
+
+        System.out.println("\nfor the deque elements:");
+        for (String str : myDeque) {
+            System.out.println(str);
+        }
+
+        System.out.println("\nhead:" + myDeque.element()); // A
+        System.out.println("head:" + myDeque.peek()); // A
+
+        System.out.println("\niterating the deque elements:");
+        Iterator itr = myDeque.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        myDeque.remove(); // M
+        myDeque.poll(); // E
+
+        System.out.println("\nafter removing two elements:");
+        Iterator<String> itr2 = myDeque.iterator();
+        while (itr2.hasNext()) {
+            System.out.println(itr2.next());
+        }
+
+        System.out.println("\nfor the deque elements:");
+        for (String str : myDeque) {
+            System.out.println(str);
+        }
+
+    }
+}
