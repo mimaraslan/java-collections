ec4b9c5e01b7f9f1bea98f1b5b7e65f00e5a1788
mimaraslan <mimaraslan@gmail.com>
2021-01-10 23:48:35 +0300

    Java Collections
    Java Collections

---
+++ src/com/mimaraslan/list/arraylist/_01_ArrayList.java
@@ -0,0 +1,80 @@
+package com.mimaraslan.list.arraylist;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+
+public class _01_ArrayList {
+	public static void main(String[] args) {
+
+		List<String> myList1 = new ArrayList<>();
+		List<String> myList2 = new LinkedList<>();
+		List<String> myList3 = new Vector<>();
+		List<String> myList4 = new Stack<>();
+
+		ArrayList<String> myList5 = new ArrayList<>();
+		LinkedList<String> myList6 = new LinkedList<>();
+		Vector<String> myList7 = new Vector<>();
+		Stack<String> myList8 = new Stack<>();
+
+		myList1.add("B");
+		myList1.add(1,"A"); // myList1.set(1,"E");  //  myList1.remove(0);  
+		myList1.add("Z");
+		myList1.add("C");
+		myList1.add("C");
+	    myList1.add(null);
+
+        
+		for (int i = 0; i < myList1.size(); i++) {
+			System.out.println(myList1.get(i));
+		}
+		
+		System.out.println("-------------------");
+
+		for (String item : myList1) {
+			System.out.println(item.toLowerCase());
+		}
+		
+		System.out.println("-------------------");
+		
+	    myList1.forEach((e) -> { 
+		      System.out.println(e);
+		    });
+		System.out.println("-------------------");
+	
+	   // Collections.sort(myList1);  
+
+		Iterator itr = myList1.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+
+		
+		System.out.println("-----forEachRemaining() method:");  
+        Iterator<String> itr2=myList1.iterator();  
+        itr2.forEachRemaining(a-> {  
+        	System.out.println(a);  
+        });  
+	}
+}
+
+
+
+/*
+
+Java ArrayList class can contain duplicate elements.
+Java ArrayList class maintains insertion order.
+Java ArrayList class is non synchronized.
+Java ArrayList allows random access because the array works on an index basis.
+
+In ArrayList, manipulation is a little bit slower than the LinkedList in Java 
+because a lot of shifting needs to occur if any element is removed from the array list.
+
+We can not create an array list of the primitive types, 
+such as int, float, char, etc. It is required to use 
+the required wrapper class in such cases. For example:		
+
+*/
