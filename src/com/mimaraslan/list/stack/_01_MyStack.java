8d150ca1f7781ccecd74bfa05b949ab58c213c9a
mimaraslan <mimaraslan@gmail.com>
2021-01-18 23:50:17 +0300

    Java Collections
    Java Collections

--- src/com/mimaraslan/list/stack/_01_MyStack.java
+++ src/com/mimaraslan/list/stack/_01_MyStack.java
@@ -3,18 +3,18 @@ package com.mimaraslan.list.stack;
 import java.util.*;
 
 public class _01_MyStack {
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 
-        Stack<String> myList = new Stack<String>();
-        myList.push("B");
-        myList.add("A");
-        myList.push("Z");
-        myList.add("C");
+		Stack<String> myList = new Stack<String>();
+		myList.push("B");
+		myList.add("A");
+		myList.push("Z");
+		myList.add("C");
 
-        Iterator<String> itr = myList.iterator();
-        while (itr.hasNext()) {
-            System.out.println(itr.next());
-        }
+		Iterator<String> itr = myList.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 
-    }
+	}
 }
--- src/com/mimaraslan/list/vector/_01_MyVector.java
+++ src/com/mimaraslan/list/vector/_01_MyVector.java
@@ -3,18 +3,18 @@ package com.mimaraslan.list.vector;
 import java.util.*;
 
 public class _01_MyVector {
-    public static void main(String args[]) {
+	public static void main(String args[]) {
 
-        Vector<String> myList=new Vector<String>();
-        myList.add("B");
-        myList.add("A");
-        myList.add("Z");
-        myList.add("C");
+		Vector<String> myList = new Vector<String>();
+		myList.add("B");
+		myList.add("A");
+		myList.add("Z");
+		myList.add("C");
 
-        Iterator<String> itr = myList.iterator();
-        while (itr.hasNext()) {
-            System.out.println(itr.next());
-        }
+		Iterator<String> itr = myList.iterator();
+		while (itr.hasNext()) {
+			System.out.println(itr.next());
+		}
 
-    }
+	}
 }
