6d77bb0a69d8aa2094d0beed2329091396e2726d
mimaraslan <mimaraslan@gmail.com>
2022-05-16 00:08:57 +0300

    Java Collections
    Java Collections

--- src/com/mimaraslan/map/MyHashMap.java
+++ src/com/mimaraslan/map/MyHashMap.java
@@ -1,5 +1,16 @@
 package com.mimaraslan.map;
+import java.util.HashMap;
 
 public class MyHashMap {
+    public static void main(String[] args) {
+        // Create a HashMap object called capitalCities
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
 
+        // Add keys and values (Country, City)
+        capitalCities.put("England", "London");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("Norway", "Oslo");
+        capitalCities.put("USA", "Washington DC");
+        System.out.println(capitalCities);
+    }
 }
--- src/com/mimaraslan/set/hashset/MyHashSet.java
+++ src/com/mimaraslan/set/hashset/MyHashSet.java
@@ -1,5 +1,18 @@
 package com.mimaraslan.set.hashset;
 
+// A HashSet is a collection of items where every item is unique, and it is found in the java.util package:
+/*
+A List can contain duplicate elements whereas Set contains unique elements only!!!
+HashSet stores the elements by using a mechanism called hashing.
+HashSet contains unique elements only.
+HashSet allows null value.
+HashSet class is non synchronized.
+HashSet doesn't maintain the insertion order.
+Here, elements are inserted on the basis of their hashcode.
+HashSet is the best approach for search operations.
+The initial default capacity of HashSet is 16, and the load factor is 0.75.
+*/
+
 import java.util.*;
 
 public class MyHashSet {
@@ -39,25 +52,3 @@ public class MyHashSet {
 	}
 }
 
-/*
-A List can contain duplicate elements 
-whereas Set contains unique elements only!!!
-
- 
- 
-HashSet stores the elements by using a mechanism called hashing.
-
-HashSet contains unique elements only.
-
-HashSet allows null value.
-
-HashSet class is non synchronized.
-
-HashSet doesn't maintain the insertion order. 
-Here, elements are inserted on the basis of their hashcode.
-
-HashSet is the best approach for search operations.
-
-The initial default capacity of HashSet is 16, and the load factor is 0.75.
- 
-*/
